2b9b7bdada2cdd7828ed700098a5d1eeac607b27
rml60 <r.m-l@gmx.de>
2021-05-24 13:07:05 +0200

    check for connection to cs. hc4067 connection modified.

--- trackstate/hc4067.py
+++ trackstate/hc4067.py
@@ -1,23 +1,52 @@
-# The MIT License (MIT)
 """ hardware.py
-    =================================================================
 
     Simple drivers for external hardware to use with esp32.
      - 74HC4067 16-channel analog/digital Multiplexer 
+     
+    gnd: 4067-pin12, vcc: 4067-pin24
+    sig: 4067-pin1,  en:  4067-pin15
+    s0:  4067-pin10, s1:  4067-pin11, s2:  4067-pin14, s3:  4067-pin13
+    y0:  4067-pin9,  y1:  4067-pin8,  y2:  4067-pin7,  y3:  4067-pin6
+    y4:  4067-pin5,  y5:  4067-pin4,  y6:  4067-pin3,  y7:  4067-pin2
+    y8:  4067-pin23, y9:  4067-pin22, y10: 4067-pin21, y11: 4067-pin20
+    y12: 4067-pin19, y13: 4067-pin18, y14: 4067-pin17, y15: 4067-pin16
 
     Author:   Rainer Maier-Lohmann
-    
-    Copyright (c) 2020 Rainer Maier-Lohmann
+    ---------------------------------------------------------------------------
+    "THE BEER-WARE LICENSE" (Revision 42):
+    <r.m-l@gmx.de> wrote this file.  As long as you retain this notice you
+    can do whatever you want with this stuff. If we meet some day, and you 
+    think this stuff is worth it, you can buy me a beer in return.
+    ---------------------------------------------------------------------------    
+    Copyright (c) 2021 
 """
 from machine import Pin
+from utime import sleep_us
 
+#-------------------------------------------------------------------------------
+# constants
+#-------------------------------------------------------------------------------
+CHECK_MAX = 100
+CHECK_MIN_ZEROS = 20
+CHECK_DELAY_US = 250
+PORTS_DEFAULT = 16
+
+#-------------------------------------------------------------------------------
+# classes
+#-------------------------------------------------------------------------------
 class Hc4067():
   """ Simple driver for 74HC4067 16-channel analog/digital Multiplexer 
   """
-  def __init__(self, pinEn, pinSig, pinS0, pinS1, pinS2, pinS3, addr=0, ports=16):
+  def __init__( self
+              , pinSig
+              , pinS0, pinS1, pinS2, pinS3
+              , addr=0
+              , pinEn = None # via hardware set to 0 - no need to configure
+              , ports=PORTS_DEFAULT
+              ):
     self.__numberOfPorts = ports
-    self.__enable = Pin(pinEn, Pin.OUT)
-    self.__signal = Pin(pinSig, Pin.IN)
+    self.__enable = Pin(pinEn, Pin.OUT) if pinEn else None
+    self.__signal = Pin(pinSig, Pin.IN, Pin.PULL_UP)
     self.__addrPins = [pinS0, pinS1, pinS2, pinS3]
     self.__pins = []
     for pin in self.__addrPins:
@@ -26,18 +55,24 @@ class Hc4067():
     self.__setAddrBits()
   
   @property
-  def signalValues(self):
-    signalValues = []
-    for addr in range(0,self.__numberOfPorts,1):
+  def portValues(self):
+    """ returns a list with all inport values.
+    """
+    portValues = []
+    for addr in range(0, self.__numberOfPorts, 1):
       self.__setAddr(addr)
-      #signalValues.append(self.__signal.value())
-      signalValues.append(self.__getRepeatedValue())
-    return signalValues
+      value = self.__getRepeatedValue()
+      portValues.append(value)
+    return portValues
 
   @property
-  def signalValueByte(self):
+  def valueByte(self):
+    """ returns a integer with the sum of all inport values according to the
+        bitposition. bitposition = address
+        ( 1 = 1 (value=2^0, ..., 16 = 16 (value=2^15) )
+    """
     valueByte = 0
-    for addr in range(0,self.__numberOfPorts,1):
+    for addr in range(0, self.__numberOfPorts, 1):
       self.__setAddr(addr)
       value = self.__getRepeatedValue()
       if value != 0:
@@ -46,37 +81,51 @@ class Hc4067():
         valueByte &= 0xFFFF - 2**addr
     return valueByte
 
-
-  def signalValue(self, addr):
+  def readPortValue(self, addr):
+    """ returns the value of the inport of the address.
+    """
     self.__setAddr(addr)
-    #return self.__signal.value()
     return self.__getRepeatedValue()
 
   def __getRepeatedValue(self):
+    """ returns the interpreted value of the selected inport.
+        Value is 1, if a minimal count of zeros were readed in a loop for
+        specified times.       
+        values: 1 - set, 0 - open 
+    """
     numberOfZeros = 0
-    for i in range(0,25):
+    bitVal = 0
+    for i in range(0, CHECK_MAX):
       val = self.__signal.value()
       if val == 0:
         numberOfZeros += 1
-    if numberOfZeros > 20:
-      val = 0
-    else:
-      val = 1
-    return val
+      if numberOfZeros >= CHECK_MIN_ZEROS:
+        bitVal = 1
+        break
+      sleep_us(CHECK_DELAY_US)
+    return bitVal
  
   def __setAddr(self, addr):
+    """ sets the address for selecting the inport.
+    """
     self.__addr = addr
     self.__setAddrBits()
     
-  def __isAddrBitSet(self, value, bitNr):
-    return ((value & 2**bitNr) >>  bitNr) == True
+  def __isAddrBitSet(self, value, bitNo):
+    """ True, if the given bit number in value is set.
+    """
+    return ((value & 2**bitNo) >> bitNo)
 
   def __setAddrBitValues(self):
+    """ sets the address bits according to the address
+    """
     self.__addrBitValues = []
     for index, pin in enumerate(self.__addrPins):
       self.__addrBitValues.append(self.__isAddrBitSet(self.__addr, index))
       
   def __setAddrBits(self):
+    """ sets the address out pins according to the address bits
+    """
     self.__setAddrBitValues()
     for pinValue, pin in zip(self.__addrBitValues, self.__pins):
       pin.value(pinValue)
--- trackstate/main.py
+++ trackstate/main.py
@@ -15,17 +15,18 @@
     ---------------------------------------------------------------------------
     (c) 2021
 """
-from machine import Timer
-import usocket
-import time
+#from machine import Timer
+from usocket import socket, AF_INET, SOCK_STREAM
+from utime import sleep_us
+from uping import ping
 
-import hc4067
+from hc4067 import Hc4067
 from mcan import mcanhash, mcancommand, states
 
 #-------------------------------------------------------------------------------
 # constants
 #-------------------------------------------------------------------------------
-VERSION = '0.05'
+VERSION = '0.06'
 TIMER = Timer(0)
 BUFFERSIZE = 13
 STATE_YPOS = 4
@@ -33,15 +34,15 @@ STATE_YPOS = 4
 #-------------------------------------------------------------------------------
 # classes and functions
 #-------------------------------------------------------------------------------
-def handleInterrupt(TIMER):
-  """
-  """
-  global currState
-  currState = 0
-  # inx.value ist entweder 0 oder 1
-  #currState = currState | in0.value()*2**0
-  #currState = currState | in1.value()*2**1
-  currState = mp.signalValueByte
+# def handleInterrupt(TIMER):
+#   """
+#   """
+#   global currState
+#   currState = 0
+#   # inx.value ist entweder 0 oder 1
+#   #currState = currState | in0.value()*2**0
+#   #currState = currState | in1.value()*2**1
+#   currState = mp.valueByte
 
 class InfoText():
   def __init__(self, display, ipAddrStr, mcanHash, csipStr, versionStr=VERSION):
@@ -52,7 +53,7 @@ class InfoText():
     #self.__hash = 'MCAN-Hash:  {}'.format(mcanHash)
     self.__ip = ipAddrStr
     self.__csip = csipStr
-    self.__csConError = True 
+    self.__csConState = False 
 
   def set(self):
     """
@@ -69,10 +70,10 @@ class InfoText():
       csipLast2Bytes = '.'+ '.'.join(csipBytes[2:])
     else:
       csipLastByte = 'Err'
-    if self.__csConError:
-      csError = 'ERR'
+    if self.__csConState:
+      csError = 'OK'
     else:
-      csError = 'con'
+      csError = 'ERR'
       
     self.__display.text('CS: {:3} {:>8}'.format(csError, csipLast2Bytes),0,42)
     self.__display.text('{:>16}'.format(self.__ip), 0, 55)
@@ -93,43 +94,75 @@ class InfoText():
     self.__csConState = newState
 
 class CsConnection():
-  def __init__(self, timeout=100):
+  def __init__(self, csIpAddr, csIpPort, timeoutSec=1):
     """
     """
-    self.__csip = CSIP    
-    self.__tcpPort = CSPORT
+    self.__csip = csIpAddr    
+    self.__tcpPort = csIpPort
+    self.__timeoutSec = timeoutSec
     print('CS-IP:',self.__csip,' Port:',self.__tcpPort)
-    self.__csConnected = False
-    try:
-      self.__socket = usocket.socket( usocket.AF_INET, usocket.SOCK_STREAM )
-      self.__socket.settimeout(timeout/1000)
-      self.__socket.connect((self.__csip, self.__tcpPort))
-      self.__csConnected = True
+    self.__connected = False
+    #try: 
       #self.__socket.setblocking(False)      
-    except Exception as e:
+    #except Exception as e:
       # EHOSTUNREACH
-      self.__csConnected = False
-      print('ERROR:', e)
+    #  self.__csConnected = False
+    #  print('ERROR:', e)
 
+  @property
+  def isConnected(self):
+    print('CS connected: {}'.format(self.__connected))
+    return self.__connected
+
+  def connect(self):
+    self.__socket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+    self.__socket.setblocking(False)
+    self.__socket.settimeout(self.__timeoutSec)
+    try:
+      self.__socket.connect((self.__csip, self.__tcpPort))
+      self.__connected = True
+    except Exception as e:
+      print('Connection error')
+      self.__connected = False
+      
+    #try:
+    #    urllib.request.urlopen(self.__csip, timeout=1)
+    #    print('CS found')
+    #    self.__socket.connect((self.__csip, self.__tcpPort))
+    #    self.__connected = True
+    #except urllib.request.URLError:
+    #    print('CS not found')
+    #    self.__connected = False
+    
+  def close(self):
+    self.__socket.close()
+    self.__connected = False
+    
   def send(self, mcanMsg):
     """
     """
-    if self.__csConnected:
+    if self.__connected:
       print('send ({}...) ...'.format(mcanMsg[:15]))
       self.__socket.send(mcanMsg)
     else:
       print('not send ({}...)!'.format(mcanMsg[:15]))
 
+def pingOk(host, count=1):
+    response = ping(host, count=count, quiet=True)
+    return response[1] == count
 #-------------------------------------------------------------------------------
 # main
 #-------------------------------------------------------------------------------
 if __name__ == '__main__':
   csConState = False
-  mp = hc4067.Hc4067( pinEn=32, pinSig=33
-                    , pinS0=12, pinS1=13, pinS2=14, pinS3=15
-                    , ports=16
-                    )
-  currState = mp.signalValueByte
+  # en:  4067-pin15 - esp32-d35-pin20  - not used 
+  # sig: 4067-pin1  - esp32-d32-pin21
+  # s0:  4067-pin10 - esp32-d12-pin27
+  # s1:  4067-pin11 - esp32-d13-pin28
+  # s2:  4067-pin14 - esp32-d14-pin26
+  # s3:  4067-pin13 - esp32-d15-pin3
+  mp = Hc4067( pinSig=32, pinS0=12, pinS1=13, pinS2=14, pinS3=15 )
+  currState = mp.valueByte
   recentState = currState
   trackstates = states.States()
 
@@ -137,29 +170,46 @@ if __name__ == '__main__':
   mcanCmd = mcancommand.McanCommand(int(mcanHash))
   mcanCmd.setCommand(0x22,response=True)
 
-  TIMER.init(period=50, mode=Timer.PERIODIC, callback=handleInterrupt)
-  csConnetion = CsConnection()
+  #TIMER.init(period=20, mode=Timer.PERIODIC, callback=handleInterrupt)
+  currState = mp.valueByte
+  csConnection = CsConnection(CSIP, CSPORT)
   
   infoText = InfoText(oled, wlan.ifconfig()[0], mcanHash, CSIP)
   infoText.set()
-
+  csConStateOk = pingOk(CSIP)
+  infoText.setCsConState(csConStateOk)
+  
   trackstates.setStateBits(currState)
+  
   oled.text(trackstates.shortStr,0, STATE_YPOS+4)
   oled.show()
   while True:
+    currState = mp.valueByte
     trackstates.setStateBits(currState)
+    print(currState)
     if trackstates.isChanged:
       infoText.set()
-      oled.text(trackstates.shortStr,0,STATE_YPOS+4)
+      csConStateOk = pingOk(CSIP)
+      infoText.setCsConState(csConStateOk)
+      oled.text(trackstates.shortStr, 0, STATE_YPOS+4)
       oled.show()
+      if csConStateOk:
+        #csConnection.connect()
+        #result = csConnection.isConnected
+        pass
       for subId, state, recentState in trackstates.changedStates:
         contactNo = CONTACTBASE + subId
         mcanCmd.setTrackState(DEVICEID, contactNo, state, recentState)
-        csConnetion.send(mcanCmd.frame)
-        print(DEVICEID, contactNo, state, mcanCmd.frame)
+        if csConStateOk:
+         #csConnection.send(mcanCmd.frame)
+         print(DEVICEID, contactNo, state, mcanCmd.frame)
+      if csConStateOk:
+        #csConnection.close()
+        pass
 
     trackstates.setRecentToCurrent()
-    time.sleep_ms(100)
+    #time.sleep_ms(100)
+    #print('new tick')
 
 #   while csConnetion is None:
 #     try:
